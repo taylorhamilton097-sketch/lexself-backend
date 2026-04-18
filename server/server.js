38e6872fbd4f9c0b69af46387e08348c870a96e0
Taylor <taylorhamilton097@gmail.com>
2026-04-17 21:07:59 -0400

    ClearStand update

--- server/server.js
+++ server/server.js
@@ -32,6 +32,7 @@ app.use(helmet({
     directives: {
       defaultSrc:    ["'self'"],
       scriptSrc:     ["'self'", "'unsafe-inline'", "https://js.stripe.com", "https://cdn.jsdelivr.net", "https://fonts.googleapis.com"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc:      ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc:       ["'self'", "https://fonts.gstatic.com"],
       imgSrc:        ["'self'", "data:", "https:"],
